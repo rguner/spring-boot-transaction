b03661cd54abe73d39f8904b078b5c4cf442d16f
Ramazan Güner <rguner@gmail.com>
2023-09-05 18:39:58 +0300

    initial data

--- src/main/java/com/guner/entity/User.java
+++ src/main/java/com/guner/entity/User.java
@@ -17,10 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String firstName;
+
     @Column(nullable = false)
     private String lastName;
+
     @Column(nullable = false, unique = true)
     private String email;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan1','Guner1','rguner@gmail.com1');
+INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan2','Guner2','rguner@gmail.com2');
+INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan3','Guner3','rguner@gmail.com3');
+INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan4','Guner4','rguner@gmail.com4');
+INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan5','Guner5','rguner@gmail.com5');
+
