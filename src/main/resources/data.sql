81274119631614cdb616e82c985d6f54e1ae293e
Ramazan Guner <ramazan.guner@haud.com>
2023-12-11 17:52:20 +0300

    running data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,7 @@ INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan3','Guner3','rgun
 INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan4','Guner4','rguner@gmail.com4');
 INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan5','Guner5','rguner@gmail.com5');
 INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan6','Guner6','rguner@gmail.com6');
+INSERT INTO users (first_name,last_name,email) VALUES ('Ramazan7','Guner7','rguner@gmail.com7');
 
 INSERT INTO product (price,title,description) VALUES ('100','Product1','Product Description1');
 INSERT INTO product (price,title,description) VALUES ('200','Product2','Product Description2');
@@ -11,3 +12,4 @@ INSERT INTO product (price,title,description) VALUES ('300','Product3','Product 
 INSERT INTO product (price,title,description) VALUES ('400','Product4','Product Description4');
 INSERT INTO product (price,title,description) VALUES ('500','Product5','Product Description5');
 INSERT INTO product (price,title,description) VALUES ('600','Product6','Product Description6');
+INSERT INTO product (price,title,description) VALUES ('700','Product7','Product Description7');
